5342151f3296a238284fc33fb65133868a56f09d
Rodrigo Parente <digus.parente@gmail.com>
2022-10-16 17:23:12 -0300

    added main

---
+++ main.py
@@ -0,0 +1,38 @@
+# python imports
+import os
+
+# project imports
+from states import start_state
+from states import generate_network
+from states import train_model
+from states import classify_vulnerability
+from states import fix_vulnerability
+from states import error_state
+
+from states.constants import START_STATE
+from states.constants import GENERATE_NETWORK
+from states.constants import TRAIN_MODEL
+from states.constants import CLASSIFY_VULNERABILITY
+from states.constants import FIX_VULNERABILITY
+from states.constants import END_STATE
+from states.constants import ERROR_STATE
+
+# local imports
+from fsm import FSM
+
+
+if __name__ == '__main__':
+
+    fsm = FSM()
+
+    fsm.add_state(START_STATE, start_state, start_state=True)
+    fsm.add_state(GENERATE_NETWORK, generate_network)
+    fsm.add_state(TRAIN_MODEL, train_model)
+    fsm.add_state(CLASSIFY_VULNERABILITY, classify_vulnerability)
+    fsm.add_state(FIX_VULNERABILITY, fix_vulnerability)
+    fsm.add_state(ERROR_STATE, error_state, error_state=True)
+    fsm.add_state(END_STATE, None, end_state=True)
+
+    config_path = os.getcwd() + '/config.ini'
+
+    fsm.run(config_path)
