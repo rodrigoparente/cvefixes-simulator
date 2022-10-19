5efa00af42de06e89ff437e4e1f3d2d97de49758
Rodrigo Parente <digus.parente@gmail.com>
2022-10-19 11:05:02 -0300

    added prioritize vulnerability state

--- states/__init__.py
+++ states/__init__.py
@@ -2,5 +2,6 @@ from .start_state import start_state  # noqa F401
 from .generate_network import generate_network  # noqa F401
 from .train_model import train_model  # noqa F401
 from .classify_vulnerability import classify_vulnerability  # noqa F401
+from .prioritize_vulnerability import prioritize_vulnerability  # noqa 401
 from .fix_vulnerability import fix_vulnerability  # noqa F401
 from .error_state import error_state  # noqa F401
